868f935cedd3eae30d3f034fb436997afd3dd323
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-12 17:09:48 -0700

    Add Person function to return JSX to render within App div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+        <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //   </div>
+    // );
+    // JSX above gets compiled to this below.
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const person = () => {
+  return(
+    <div className='Person'>
+      <h1>Jamie</h1>
+      <p>My age is 23</p>
+    </div>
+  );
+}
+
+export default person;
