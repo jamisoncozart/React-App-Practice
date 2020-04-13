7654c39afe0aa48720c042c0b4de3895fcfda94f
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-13 07:34:00 -0700

    Add event listener to button that changes state

--- src/App.js
+++ src/App.js
@@ -6,19 +6,32 @@ class App extends Component {
   state = {
     persons: [
       { name: 'Jamie', age: 23 },
-      { name: 'Nichole', age: 22 },
+      { name: 'Nico', age: 22 },
       { name: 'Pat', age: 66 },
       { name: 'Jim', age: 76 }
     ]
   }
 
+  switchNameHandler = () => {
+    // this.state.persons[0].name="Jamison"; DONT TO THIS
+    this.setState({
+      persons: [
+        { name: 'Jamison', age: 23 },
+        { name: 'Nichole', age: 22 },
+        { name: 'Patricia', age: 66 },
+        { name: 'James', age: 76 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <button id="switchName">Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
       </div>
     );
     // return (
